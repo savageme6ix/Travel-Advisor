a715b89c34820f3ddc19846eb1b722754040bcde
savageme6ix <maingithomas803@gmail.com>
2026-04-19 21:01:48 +0300

    Initial layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import Header from "./Components/Header/Header"
 import List from "./Components/List/List"
 import Map from "./Components/Map/Map"
-import {CssBaseline, Grid} from '@mui/material/core'
+import {CssBaseline, Grid} from '@mui/material';
 const App = () => {
   return (
-    <div> <Header /> </div>
+       <>
+        <CssBaseline />
+        <Header />
+        <Grid container spacing={3} style={{width:'100%'}}>
+            <Grid item xs={12} md={4}>
+              <List />
+            </Grid>
+            <Grid item xs={12} md={8}>
+              <Map />
+            </Grid>
+        </Grid>
+       </>
   )
 }
 
