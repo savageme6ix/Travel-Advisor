821498e34514747c789f000ac22a0db62c679477
savageme6ix <maingithomas803@gmail.com>
2026-04-22 12:32:48 +0300

    Get user coordinates using browser built in geolocator

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,34 @@
 import Header from "./Components/Header";
 import List from "./Components/List";
 import Map from "./Components/Map";
-import { Box , CssBaseline} from "@mui/material";
+import { Box, CssBaseline } from "@mui/material";
+import { useState, useEffect } from "react";
 
 const App = () => {
+  //Get user coordinates using browser built in geolocator
+  const [coordinates, setCoordinates] = useState({
+    lat: 0,
+    lng: 0,
+  });
+
+  useEffect(function () {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      setCoordinates({
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      });
+    });
+  }, []);
+
   return (
     <>
-    <CssBaseline />
-    <Header />
-    <Box sx={{ display: "flex", height: "90vh" }}>
-      <Box sx={{ width: "30%" }}>LIST</Box>
+      <CssBaseline />
+      <Header />
+      <Box sx={{ display: "flex", height: "90vh" }}>
+        <Box sx={{ width: "30%" }}>LIST</Box>
 
-      <Box sx={{ width: "70%" }}>MAP</Box>
-    </Box>
+        <Box sx={{ width: "70%" }}>MAP</Box>
+      </Box>
     </>
   );
 };
