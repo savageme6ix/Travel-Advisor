8de933a2f87917ebec8d42eb47ec7dd87e2462b6
savageme6ix <maingithomas803@gmail.com>
2026-04-22 12:26:36 +0300

    Added css baseline for browser inconsisitencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import Header from "./Components/Header";
 import List from "./Components/List";
 import Map from "./Components/Map";
-import { Box } from "@mui/material";
+import { Box , CssBaseline} from "@mui/material";
 
 const App = () => {
   return (
+    <>
+    <CssBaseline />
+    <Header />
     <Box sx={{ display: "flex", height: "90vh" }}>
       <Box sx={{ width: "30%" }}>LIST</Box>
 
       <Box sx={{ width: "70%" }}>MAP</Box>
     </Box>
+    </>
   );
 };
 
